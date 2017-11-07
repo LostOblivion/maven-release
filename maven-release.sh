b0fdc5af560fe556d7ac464bb7bc63527aa2e2c3
Olai Solheim <olai.solheim@gmail.com>
2017-11-07 21:28:05 +0100

    Meldinger.

--- maven-release.sh
+++ maven-release.sh
@@ -47,13 +47,21 @@ function main() {
         return -1
     fi
 
+    echo "Setting the project version ..."
     maven_set_version $(get_date_version)
 
-    mvn clean install -B -P release-profile
+    echo "Building the project with the following command:"
+    echo mvn $@
 
+    mvn $@
+
+    echo "Committing, tagging, and pushing ..."
     git commit -a -m "$message Preparing to release $(maven_evaluate project.groupId):$(maven_evaluate project.artifactId):$(maven_evaluate project.version)"
+    git tag $(maven_evaluate project.version)
+    git push
+    git push --tags
 }
 
 set -e
-main
+main $*
 exit $?
